1357cdc221ca3b606e19d0e5a4a815933aba9791
Nicolai Berk <nicolai.berk@gmail.com>
2021-03-27 10:30:27 +0100

    Improve vis for midterm report.

--- 03_Vis.R
+++ 03_Vis.R
@@ -28,7 +28,9 @@ plot <- function(party, fillcol) {
   ggplot(dta) +
     geom_histogram(aes_string(x = party), fill = fillcol) +
     facet_grid(~source) +
-    xlab(party)
+    xlab(party) + 
+    scale_x_continuous(limits = c(0,1),
+                       breaks = c(0, 0.5))
   
 }
 
@@ -39,5 +41,7 @@ spd    <- plot('spd',   'red')
 linke  <- plot('linke', 'darkred')
 fdp    <- plot('fdp',   'yellow')
 
-grid.arrange(linke, greens, spd, union, fdp, afd) %>% 
-  ggsave(filename = 'preds.png', plot = ., width = 15, height = 7.5)
+grid.arrange(linke, greens, spd, union, fdp, afd,
+             ncol=1) %>% 
+  ggsave(filename = 'reports/midterm-report-latex/figures/preds.png', plot = ., 
+         width = 5, height = 10)
